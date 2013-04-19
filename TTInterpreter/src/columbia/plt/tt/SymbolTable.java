7d0e04d30292f30f4bc633bce87bc0897ec8e254
Jason Lee <jslee0813@hotmail.com>
2013-04-18 21:38:37 -0300

    Create SymbolTable.java

---
+++ TTInterpreter/src/columbia/plt/tt/SymbolTable.java
@@ -0,0 +1,42 @@
+package columbia.plt.tt;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class SymbolTable extends Stack<Scope> {
+
+  private int nextScopeID;
+
+    public SymbolTable() {
+      this.push(new Scope(0));  // add a global scope
+      nextScopeID = 1;
+    }
+
+    public Scope getCurrentScope()
+    {
+       if (this.size() > 0) 
+             return this.peek();
+         else
+           return null;            	 
+    }
+
+    public Scope getScope(int scopeID) {
+        for (Scope scope : this) {
+            if (scope.getID() == scopeID)
+              return scope;
+        }
+        
+        return null;
+    }
+
+    public int addScope() {
+      this.push(new Scope(nextScopeID));
+      nextScopeID++;
+      return nextScopeID - 1;
+    }   
+    
+}
