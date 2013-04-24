00c76dcef07d7b5ed2cb662764ec73f66919b3ce
Jason Lee <jslee0813@hotmail.com>
2013-04-23 21:14:44 -0300

    Update TTGrammarTester.java

--- TTInterpreter/src/columbia/plt/tt/TTGrammarTester.java
+++ TTInterpreter/src/columbia/plt/tt/TTGrammarTester.java
@@ -1,5 +1,9 @@
 package columbia.plt.tt;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
@@ -14,23 +18,31 @@ import columbia.plt.tt.TTGrammarParser.everyFromToByStatement_return;
 
 public class TTGrammarTester {
 
-  public static void main(String[] args) throws RecognitionException {		
-  	//CharStream stream = new ANTLRStringStream(	"Calendar x;");
-	  CharStream stream = new ANTLRStringStream( "Calendar x; every Date d from 2013.01.01 to 2013.01.03 by tf { }");
-  	TTGrammarLexer lexer = new TTGrammarLexer(stream);
-  	TokenStream tokenStream = new CommonTokenStream(lexer);
-  	TTGrammarParser parser = new TTGrammarParser(tokenStream);
+  public static void main(String[] args) throws RecognitionException {
+    try {
+      CharStream stream = new ANTLRInputStream(new FileInputStream("/Program.tt"));
+      //CharStream stream = new ANTLRStringStream(	"Calendar x;");
+      //CharStream stream = new ANTLRStringStream( "Calendar x; every Date d from 2013.01.01 to 2013.01.03 by tf { }");
+      TTGrammarLexer lexer = new TTGrammarLexer(stream);
+      TokenStream tokenStream = new CommonTokenStream(lexer);
+      TTGrammarParser parser = new TTGrammarParser(tokenStream);
   	
-  	declarationStatement_return declaration = parser.declarationStatement();
-  	System.out.println(declaration.tree.toStringTree());
+      declarationStatement_return declaration = parser.declarationStatement();
+      System.out.println(declaration.tree.toStringTree());
   	
-  	everyFromToByStatement_return every = parser.everyFromToByStatement();
-  	System.out.println(every.tree.toStringTree());
+      everyFromToByStatement_return every = parser.everyFromToByStatement();
+      System.out.println(every.tree.toStringTree());
   	
-  	TTGrammarEvaluator evaluator = new TTGrammarEvaluator(new CommonTreeNodeStream(every.tree));//declaration.tree));
+      TTGrammarEvaluator evaluator = new TTGrammarEvaluator(new CommonTreeNodeStream(every.tree));//declaration.tree));
   	//evaluator.declarationStatement();
-  	evaluator.everyFromToByStatement();
+      evaluator.everyFromToByStatement();
+    } 
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
   }
-
 }
 
