ea917356f2a0472857317fe4e2ebd0cd093af6ff
Jason Lee <jslee0813@hotmail.com>
2013-05-03 22:36:15 -0300

    Update Scope.java

--- TTInterpreter/src/columbia/plt/tt/interpreter/Scope.java
+++ TTInterpreter/src/columbia/plt/tt/interpreter/Scope.java
@@ -6,15 +6,27 @@ import java.lang.String;
 public class Scope extends HashMap<String, Symbol> {
   
     private final int id;
-  
+    private final boolean isChild;
+    
     public Scope(final int id) {
         this.id = id;
+        this.isChild = false;
     }
     
+    public Scope(final int id, boolean isChild)
+    {
+        this.id = id;
+        this.isChild = isChild;
+    }
+            
     public int getID() {
       return this.id;
     }
     
+    public boolean getIsChild() {
+      return this.isChild;
+    }
+        
     public void addSymbol(String name, String type, Object value) {
       this.put(name, new Symbol(type, value));
     }
